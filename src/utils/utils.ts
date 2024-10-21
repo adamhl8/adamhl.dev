9e450e36b2e135f8f55eec897454cfbbf60a28d3
Adam Langbert <adamhl@pm.me>
2024-10-21 14:20:08 -0500

    fix projects

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -8,4 +8,13 @@ async function getSortedCollection<T extends CollectionNames>(collection: T) {
   return posts.sort((a, b) => b.data.date.valueOf() - a.data.date.valueOf())
 }
 
-export { getSortedCollection }
+let languageColors: Record<string, { color: string }>
+
+async function getLanguageColors() {
+  if (languageColors) return languageColors
+  const colorsResp = await fetch("https://raw.githubusercontent.com/ozh/github-colors/master/colors.json")
+  languageColors = await colorsResp.json()
+  return languageColors
+}
+
+export { getSortedCollection, getLanguageColors }
