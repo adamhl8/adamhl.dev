39df8b9c9885a1bd8dd739d5e23a44380a959b2f
Adam Langbert <adamhl@pm.me>
2024-10-22 21:00:10 -0500

    wip

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,8 @@
 import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap"
 import tailwind from "@astrojs/tailwind"
+import { pluginLineNumbers } from "@expressive-code/plugin-line-numbers"
+import expressiveCode from "astro-expressive-code"
 import icon from "astro-icon"
 import { defineConfig } from "astro/config"
 import rehypeCallouts from "rehype-callouts"
@@ -11,6 +13,16 @@ import { remarkReadingTime } from "./src/utils/remark-reading-time.ts"
 export default defineConfig({
   site: "https://adamhl.dev",
   integrations: [
+    expressiveCode({
+      themes: ["github-dark", "github-light"],
+      themeCssSelector: () => {
+        return `[data-theme="light"]`
+      },
+      plugins: [pluginLineNumbers()],
+      defaultProps: {
+        wrap: true,
+      },
+    }),
     mdx(),
     sitemap({
       filter: (page) => !page.includes("/share/"),
