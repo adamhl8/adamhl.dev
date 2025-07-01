20be8daa374f883adccad820680e40b876414a76
Adam Langbert <adamhl@pm.me>
2025-07-01 11:17:42 -0500

    use biome

--- prettier.config.js
+++
@@ -1,8 +0,0 @@
-import prettierConfig from "@adamhl8/configs/prettier"
-
-/** @type {import("prettier").Config} */
-// biome-ignore lint/style/noDefaultExport: prettier config
-export default {
-  ...prettierConfig,
-  tailwindAttributes: ["titleClass"],
-}
