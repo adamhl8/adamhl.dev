aabd0b77da7eece92dd5afeffeece102efd31d32
Adam Langbert <adamhl@pm.me>
2025-10-01 10:51:56 -0500

    fix CI

--- src/pages/_projects/_RepoCard/utils.ts
+++ src/pages/_projects/_RepoCard/utils.ts
@@ -10,28 +10,21 @@ const RepoSchema = v.object({
 })
 
 /**
- * Fetches the repo data from the GitHub API using `GITHUB_TOKEN` if available,
- * otherwise falls back to unauthenticated requests (rate limited)
+ * Fetches the repo data from the GitHub API
  *
  * @param repo The repo name: e.g. "adamhl8/my-repo"
  * @returns The repo data: {@link RepoSchema}
  */
 export async function getRepoData(repo: string) {
-  // fallback to unauthenticated requests if GITHUB_TOKEN is not set so this works in CI
-  const repoFetchOptions =
-    typeof import.meta.env["GITHUB_TOKEN"] === "string"
-      ? {
-          headers: {
-            Accept: "application/vnd.github+json",
-            Authorization: `Bearer ${import.meta.env["GITHUB_TOKEN"]}`,
-            "X-GitHub-Api-Version": "2022-11-28",
-          },
-        }
-      : {}
-
-  const repoResp = await fetch(`https://api.github.com/repos/${repo}`, repoFetchOptions)
+  const repoResp = await fetch(`https://api.github.com/repos/${repo}`, {
+    headers: {
+      Accept: "application/vnd.github+json",
+      Authorization: `Bearer ${import.meta.env["GITHUB_TOKEN"]}`,
+      "X-GitHub-Api-Version": "2022-11-28",
+    },
+  })
   if (!repoResp.ok)
-    throw new Error(`Failed to fetch repo data for ${repo}: ${repoResp.status.toString()} ${repoResp.statusText}`)
+    throw new Error(`failed to fetch repo data for ${repo}: ${repoResp.status.toString()} ${repoResp.statusText}`)
 
   return v.parse(RepoSchema, await repoResp.json())
 }
